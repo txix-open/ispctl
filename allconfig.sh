6186eb2786d758baf82590abe99fff2f00635bf7
DeniskaRediska <grj.den@gmail.com>
2019-09-06 18:31:00 +0300

    update(script schema response to html)

--- allconfig.sh
+++ allconfig.sh
@@ -6,20 +6,21 @@ else
   statusResponse=$(isp-ctl status)
 fi
 
-firstRecordSkip="MODULE"
-secondRecordSkip="+------------------------+---------------+-------------------------------+"
+i=0
 IFS=$'\n'
 for status in $statusResponse; do
-  IFS=$'  '
-  read -ra ADDR <<<"$status"
-  for module_name in "${ADDR[0]}"; do
-    if [ "$module_name" != "$firstRecordSkip" ] && [ "$module_name" != "$secondRecordSkip" ]; then
+  if [ $i -ne 2 ]; then
+    i=$((i + 1))
+  else
+    IFS=$'  '
+    read -ra ADDR <<<"$status"
+    for module_name in "${ADDR[0]}"; do
       if [ -n "$*" ]; then
         echo $(isp-ctl $* get $module_name .) >configs/$module_name.json
       else
         echo $(isp-ctl get $module_name .) >configs/$module_name.json
       fi
-    fi
-    break
-  done
+      break
+    done
+  fi
 done
--- allschema.sh
+++ allschema.sh
@@ -6,20 +6,23 @@ else
   statusResponse=$(isp-ctl status)
 fi
 
-firstRecordSkip="MODULE"
-secondRecordSkip="+------------------------+---------------+-------------------------------+"
+i=0
 IFS=$'\n'
 for status in $statusResponse; do
-  IFS=$'  '
-  read -ra ADDR <<<"$status"
-  for module_name in "${ADDR[0]}"; do
-    if [ "$module_name" != "$firstRecordSkip" ] && [ "$module_name" != "$secondRecordSkip" ]; then
-      if [ -n "$*" ]; then
-        echo $(isp-ctl $* schema $module_name) >schemas/$module_name.json
-      else
-        echo $(isp-ctl schema $module_name) >schemas/$module_name.json
-      fi
-    fi
-    break
-  done
+  if [ $i -ne 2 ]; then
+    i=$(( i + 1 ))
+  else
+    IFS=$'  '
+    read -ra ADDR <<<"$status"
+    for module_name in "${ADDR[0]}"; do
+
+        if [ -n "$*" ]; then
+          echo $(isp-ctl $* schema $module_name -o html) >schemas/$module_name.html
+        else
+          echo $(isp-ctl schema $module_name -o html) >schemas/$module_name.html
+        fi
+
+      break
+    done
+  fi
 done
